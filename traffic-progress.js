1d770cc977a611af22bb035d44263f734412dc93
chhb520 <chhb3675@163.com>
2025-08-19 16:50:03 +0800

    Update traffic-progress.js

--- traffic-progress.js
+++ traffic-progress.js
@@ -6,7 +6,7 @@ function injectCustomCSS() {
   style.textContent = `
     /* 隐藏父级类名为 mt-4 w-full mx-auto 下的所有 div */
     .mt-4.w-full.mx-auto > div {
-      display: block !important;
+      display: none;
     }
   `;
   document.head.appendChild(style);
